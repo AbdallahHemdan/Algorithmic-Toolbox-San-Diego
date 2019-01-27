8839e8b4a71a3ed402edfa87b70519403f698a29
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-27 12:01:14 +0200

    Create change.cpp

---
+++ week3_programming_challenges/1_money_change/change.cpp
@@ -0,0 +1,66 @@
+/*Author : Abdallah Hemdan */
+/***********************************************/
+/* Dear online judge:
+* I've read the problem, and tried to solve it.
+* Even if you don't accept my solution, you should respect my effort.
+* I hope my code compiles and gets accepted.
+*  ___  __
+* |\  \|\  \
+* \ \  \_\  \
+*  \ \   ___ \emdan
+*   \ \  \\ \ \
+*    \ \__\\ \_\
+*     \|__| \|__|
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include <array>
+#include <tuple>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+#define     endl                     '\n'
+
+typedef   long long int               ll;
+
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+
+using namespace std;
+int main() {
+	int Val; 
+	cin >> Val; 
+	int Sum = 0; 
+	Sum += Val / 10; 
+	Sum += (Val % 10) / 5; 
+	Sum += ((Val % 10) % 5);
+	cout << Sum << endl;
+}
