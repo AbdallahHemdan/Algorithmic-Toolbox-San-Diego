5d371d358cec6d62e874534716c1736a9ec5d8c0
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-27 13:18:42 +0200

    Create fibonacci_last_digit.cpp

---
+++ week2_programming_challenges/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -0,0 +1,72 @@
+/*Author : Abdallah Hemdan */
+/***********************************************/
+/* Dear online judge:
+* I've read the problem, and tried to solve it.
+* Even if you don't accept my solution, you should respect my effort.
+* I hope my code compiles and gets accepted.
+*  ___  __
+* |\  \|\  \
+* \ \  \_\  \
+*  \ \   ___ \emdan
+*   \ \  \\ \ \
+*    \ \__\\ \_\
+*     \|__| \|__|
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include <array>
+#include <tuple>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+#define     endl                     '\n'
+
+typedef   long long int               ll;
+
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+using namespace std;
+ll fibCalc(int n)
+{
+	vector<ll> fib(n + 1);
+	fib[0] = 0;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++)
+		fib[i] = (fib[i - 1]%10 + fib[i - 2]%10)%10;
+	return fib[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fibCalc(n) << endl;
+}
