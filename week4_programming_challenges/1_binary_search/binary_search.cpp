728d033f563e310cd7e23bee4880c36d8e2bc8e7
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-28 11:13:31 +0200

    Update binary_search.cpp

--- week4_programming_challenges/1_binary_search/binary_search.cpp
+++ week4_programming_challenges/1_binary_search/binary_search.cpp
@@ -14,36 +14,6 @@
 */
 
 #include <iostream>
-#include <cmath>
-#include <string>
-#include <string.h>
-#include <stdlib.h>
-#include <algorithm>
-#include <iomanip>
-#include <assert.h>
-#include <vector>
-#include <cstring>
-#include <map>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <set>
-#include <complex>
-#include <list>
-#include <climits>
-#include <cctype>
-#include <bitset>
-#include <numeric>
-#include <array>
-#include <tuple>
-#include <stdexcept>
-#include <utility>
-#include <functional>
-#include <locale>
 #define     all(v)            v.begin(),v.end()
 #define     mp                     make_pair
 #define     pb                     push_back
