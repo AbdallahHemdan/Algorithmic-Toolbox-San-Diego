02cc65530b199f1b023ad655b6002d721e88a48a
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-28 10:57:52 +0200

    Create binary_search.cpp

---
+++ week4_programming_challenges/1_binary_search/binary_search.cpp
@@ -0,0 +1,93 @@
+/*Author : Abdallah Hemdan */
+/***********************************************/
+/* Dear online judge:
+* I've read the problem, and tried to solve it.
+* Even if you don't accept my solution, you should respect my effort.
+* I hope my code compiles and gets accepted.
+*  ___  __
+* |\  \|\  \
+* \ \  \_\  \
+*  \ \   ___ \emdan
+*   \ \  \\ \ \
+*    \ \__\\ \_\
+*     \|__| \|__|
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include <array>
+#include <tuple>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+#define     endl                     '\n'
+
+typedef   long long int               ll;
+
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+
+
+using namespace std;
+
+int BinarySearch(vector<int> Arr, int n, int Target) {
+	int l = 0, r = n - 1;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (Arr[mid] == Target)
+			return mid;
+		else if (Arr[mid] < Target) {
+			l = mid + 1; 
+		}
+		else if (Arr[mid] > Target) {
+			r = mid - 1;
+		}
+	}
+	return -1; 
+}
+void FindinFirst(vector<int> First, int n, vector<int> Second, int k) {
+	for (int i = 0; i < k; i++)
+		cout << BinarySearch(First,n, Second[i]) << " ";
+}
+int main() {
+	int n; 
+	cin >> n;
+	vector<int> First(n); 
+	for (size_t i = 0; i < n; i++)
+		cin >> First[i];
+
+	int k;
+	cin >> k;
+	vector<int> Second(k);
+	for (size_t i = 0; i < k; i++)
+		cin >> Second[i];
+
+	FindinFirst(First, n, Second, k);
+}
