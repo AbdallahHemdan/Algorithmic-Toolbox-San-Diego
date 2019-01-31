176017f087cdd4f7ff4779c76301908d28c9adcb
Abdallah Hemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-31 19:38:23 +0200

    Create edit_distance.cpp

---
+++ week5_programming_challenges/3_edit_distance/edit_distance.cpp
@@ -0,0 +1,88 @@
+/*Author : Abdallah Hemdan */
+/***********************************************/
+/* Dear online judge:
+* I've read the problem, and tried to solve it.
+* Even if you don't accept my solution, you should respect my effort.
+* I hope my code compiles and gets accepted.
+*  ___  __
+* |\  \|\  \
+* \ \  \_\  \
+*  \ \   ___ \emdan
+*   \ \  \\ \ \
+*    \ \__\\ \_\
+*     \|__| \|__|
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include <array>
+#include <tuple>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <locale>
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define pb push_back
+#define endl '\n'
+
+typedef long long int ll;
+
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+using namespace std;
+
+
+int editDistDP(string str1, string str2, int m, int n) {
+	vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+	for (int i = 0; i <= m; i++) {
+		for (int j = 0; j <= n; j++) {
+			if (i == 0)
+				dp[i][j] = j;
+
+			else if (j == 0)
+				dp[i][j] = i;
+
+			else if (str1[i - 1] == str2[j - 1])
+				dp[i][j] = dp[i - 1][j - 1];
+
+			else
+				dp[i][j] = 1 + min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]);
+		}
+	}
+
+	return dp[m][n];
+}
+int main() {
+	string First_String, Second_String;
+	cin >> First_String >> Second_String;
+	
+	cout << editDistDP(First_String, Second_String, First_String.size(),
+		Second_String.size())
+		<< endl;
+}
