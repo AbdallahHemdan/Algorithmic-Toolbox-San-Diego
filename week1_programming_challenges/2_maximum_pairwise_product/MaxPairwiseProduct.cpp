1786d8cf222546efa9ec8532f5521dd5194bfe0b
Vaibhav Dubewar <70598869+vaibhavd21@users.noreply.github.com>
2020-10-01 12:22:25 +0530

    Update MaxPairwiseProduct.cpp

--- week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
+using namespace std;
 
-using namespace std; 
+long long MaxPairwiseProduct(int * numbers,int n)
+{
+  int max_index1 = -1;
+  for (int i = 0; i < n; ++i)
+      if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+          max_index1 = i;
 
-// Function To Calc The Max Product
-long long int MaxPairwiseProductFast(vector<int> arr, int n) {
-	sort(arr.begin(), arr.end()); 
-	return (long long int)(arr[n - 1]) * (arr[n - 2]); 
+  int max_index2 = -1;
+  for (int j = 0; j < n; ++j)
+      if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+          max_index2 = j;
+
+  return ((long long) (numbers[max_index1])) * numbers[max_index2];
 }
 
-int main() {
-	int n; 
-	cin >> n; 
-	vector<int> arr(n);
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i]; 
-	}
-	long long int Res = MaxPairwiseProductFast(arr, n);
-	cout << Res << endl; 
+int main()
+{
+  int n;
+  cin>>n;
+  int arr[n];
+  for (int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+
+  long long result=MaxPairwiseProduct(arr,n);
+  cout<<result<<endl;
+  return 0;
 }
+
